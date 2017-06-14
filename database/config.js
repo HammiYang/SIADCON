cd6055ac1cf8a10d7da48edd41341f90bcd793ee
Hammi Muñiz <Hammi Muiz>
2017-06-13 21:38:58 -0500

    Cambios

--- database/config.js
+++ database/config.js
@@ -1,5 +1,6 @@
 var config = {
-   "url": process.env.DB || "mongodb://Hammi:1903@ds127132.mlab.com:27132/siadcon" 
+   "url": process.env.DB || "mongodb://Hammi:1903cls
+   @ds127132.mlab.com:27132/siadcon" 
     //"url": process.env.DB || "mongodb://127.0.0.1:27017/Condominio" 
     
 };
